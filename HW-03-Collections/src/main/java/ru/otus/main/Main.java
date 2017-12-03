7441207f032d730ca39fd5641fb54a6ef89c0575
SGE <gsm-it@yandex.ru>
2017-12-03 22:34:39 +0400

    Home work 03 (Collections). Initial version.

---
+++ HW-03-Collections/src/main/java/ru/otus/main/Main.java
@@ -0,0 +1,62 @@
+package ru.otus.main;
+
+import ru.otus.utils.*;
+import java.util.*;
+
+/**
+ * Created by Gregory Smolyanov.
+ * <p>
+ * Home work 03 - ArrayList realisation
+ */
+
+public class Main {
+  private static final int INITIAL_LIST_SIZE = 8;
+
+  private static void printListElements(List list) {
+    if (list != null) {
+      StringBuffer sb = new StringBuffer();
+      sb.append("[");
+      for(Object obj: list.toArray()) {
+        if (sb.toString().compareTo("[")!=0)
+          sb.append(", ");
+        sb.append(obj);
+      }
+      sb.append("]");
+      System.out.println(sb.toString());
+    } else {
+      System.out.println("Error: List is null");
+    }
+  }
+
+  public static void main(String[] args) {
+    List<Integer> myList = new MyArrayList<Integer>();
+
+    // List initializing
+    Random rnd = new Random();
+    for (int i = 1; i <= INITIAL_LIST_SIZE; i++) {
+      myList.add(rnd.nextInt(100));
+    }
+
+    System.out.println("List initialized by values:");
+    printListElements(myList);
+
+    // List sorting
+    Collections.sort(myList, (integer, t1) ->
+        Integer.compare(integer, t1)
+    );
+    System.out.println("List elements after sorting:");
+    printListElements(myList);
+
+    // List copying
+    List<Integer> arrList = new ArrayList<>();
+    for (int i = 0; i < myList.size(); i++) arrList.add(0);
+    Collections.copy(arrList, myList);
+    System.out.println("Copied list elements:");
+    printListElements(arrList);
+
+    // List enlargement
+    System.out.println("List elements after adding any items:");
+    Collections.addAll(myList, 111, 222, 333 ,444, 555);
+    printListElements(myList);
+  }
+}
---
+++ HW-03-Collections/src/main/java/ru/otus/utils/MyArrayList.java
@@ -0,0 +1,429 @@
+package ru.otus.utils;
+
+import java.util.*;
+
+/**
+ * Created by Gregory Smolyanov.
+ * <p>
+ * Home work 03 - ArrayList realisation
+ */
+public class MyArrayList<T> implements List<T>{
+
+  private static final Object[] EMPTY_DATA = {};
+
+  // текущий размер массива
+  private int size;
+
+  // Массив элементов
+  private Object[] elements;
+
+  // размер списка по умолчанию
+  private static final int DEFAULT_CAPACITY = 10;
+
+  private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
+  private String getIndexOutOfBoundsMess(int index) {
+    return ("Index: "+index+", Size: "+this.size);
+  }
+
+  // проверка допустимого значения индекса
+  private void rangeCheck(int index) {
+    if (index < 0 || index >= this.size)
+      throw new IndexOutOfBoundsException(getIndexOutOfBoundsMess(index));
+  }
+
+  // проверка и при необходимости увеличение размера списка
+  private void checkAndGrowCapacity(int newCount) {
+    int newSize = Math.max(size+newCount, DEFAULT_CAPACITY);
+    int currentLength = elements.length;
+
+    if (newSize > currentLength) {
+      int newCap = currentLength + (currentLength >> 1); // K ~ 1.5
+
+      if (newCap < newSize) newCap = newSize;
+
+      if (newCap < newCount) newCap = newCount;
+
+      if (newCap > MAX_ARRAY_SIZE) newCap = MAX_ARRAY_SIZE;
+
+      elements = Arrays.copyOf(elements, newCap);
+    }
+  }
+
+  private class Iter implements Iterator<T> {
+    private int pos = 0;
+
+    public boolean hasNext() {
+      return pos != size;
+    }
+
+    public T next() {
+      if (pos >= size)
+        throw new NoSuchElementException();
+      return (T) elements[pos++];
+    }
+  }
+
+  /**
+   *  Конструктор по умолчанию.
+   */
+  public MyArrayList() {
+    this.elements = new Object[DEFAULT_CAPACITY];
+    size = 0;
+  }
+
+  /**
+   *  Конструктор с указанием начального размера списка.
+   */
+  public MyArrayList(int initialCapacity) {
+    if (initialCapacity > 0) {
+      this.elements = new Object[initialCapacity];
+    } else if (initialCapacity == 0) {
+      this.elements = EMPTY_DATA;
+      size = 0;
+    }
+    throw new IllegalArgumentException("Illegal capacity: "+initialCapacity);
+  }
+
+  /**
+   * Возвращает элемент с указанным индексом.
+   * */
+  public T get(int index) {
+    rangeCheck(index);
+    return (T) elements[index];
+  }
+
+  /**
+   * Замещает элемент с указанным индексом на указанный элемент.
+   * Возврашает замещенный элемент.
+   */
+  public T set(int index, T element) {
+    rangeCheck(index);
+
+    T oldValue = (T) elements[index];
+    elements[index] = element;
+    return oldValue;
+  }
+
+  /**
+   * Возвращает текущее число элементов массива.
+   * */
+  public int size() {
+    return size;
+  }
+
+  /**
+   * Возвращает true, если список пустой, иначе false
+   * */
+  public boolean isEmpty(){
+    return size == 0;
+  }
+
+  /**
+   * Возвращает наименьший индекс указанного объекта,
+   * если такой объект не найден, возвращает -1.
+   * */
+  public int indexOf(Object o) {
+    if (o == null) {
+      for (int i=0; i<size; i++)
+        if (elements[i]==null)
+          return i;
+    } else {
+      for (int i=0; i<size; i++) {
+        if (o.equals(elements[i])) return i;
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * Возвращает наибольший индекс указанного объекта,
+   * если такой объект не найден, возвращает -1.
+   * */
+  public int lastIndexOf(Object o){
+    if (o == null) {
+      for (int i = size-1; i >= 0; i--)
+        if (elements[i] == null)
+          return i;
+    } else {
+      for (int i = size-1; i >= 0; i--)
+        if (o.equals(elements[i]))
+          return i;
+    }
+    return -1;
+  }
+
+  /*
+  * метод  не реализован
+  * */
+  public ListIterator<T> listIterator() {
+    throw new RuntimeException("Not realised");
+  }
+
+  /*
+  * метод  не реализован
+  * */
+  public ListIterator<T> listIterator(int i) {
+    throw new RuntimeException("Not realised");
+  }
+
+  /*
+  * метод  не реализован
+  * */
+  public List<T> subList(int i, int i1) {
+    throw new RuntimeException("Not realised");
+  }
+
+  /**
+   * Возвращает true, если указанный объект содержится в списке, иначе возвращает false.
+   * */
+  public boolean contains(Object o){
+    return indexOf(o) >= 0;
+  }
+
+  /**
+   * Возвращает итератор по списку
+   */
+  public Iterator<T> iterator(){
+    return new Iter();
+  }
+
+  /**
+   * Возвращает массив, содержащий все элементы списка в точной последовательности.
+   */
+  public Object[] toArray(){
+    return Arrays.copyOf(elements, size);
+  }
+
+  /**
+   * Возвращает элементы списка в виде массива типа T
+   */
+  public <T> T[] toArray(T[] a){
+    if (a.length >= size) {
+      System.arraycopy(elements, 0, a, 0, size);
+      for (int i = size; i < a.length; i++) {
+        a[i] = null;
+      }
+    } else {
+      // длина a меньше elements
+      // создаём и возвращаем массив нужного размера
+      return (T[]) Arrays.copyOf(elements, size, a.getClass());
+    }
+    return a;
+  }
+
+  /**
+   * добавление элемента в конец списка
+   */
+  public boolean add(T e){
+    checkAndGrowCapacity(1);
+    elements[size++] = e;
+    return true;
+  }
+
+  /**
+   * добавление элемента в указанную позицию в списке
+   */
+  public void add(int index, T e) {
+    checkAndGrowCapacity(1);
+    // увеличение размера массива
+    size++;
+    elements = Arrays.copyOf(elements, size);
+    // сдвиг элементов
+    System.arraycopy(elements, index, elements, index + 1,size - index - 1);
+    // установка значения
+    elements[index] = e;
+  }
+
+  /**
+   * Удаляет элемент из списка.
+   */
+  public boolean remove(Object o){
+    if (o == null) {
+      for (int i = 0; i < size; i++) {
+        if (elements[i] == null) {
+          // количество переносимых элементов
+          int cntToMove = size - i - 1;
+          if (cntToMove > 0) {
+            System.arraycopy(elements, i+1, elements, i, cntToMove);
+            size--;
+            elements[size] = null;
+          }
+
+          return true;
+        }
+      }
+    } else {
+      for (int i = 0; i < size; i++) {
+        if (elements[i].equals(o)) {
+          // количество переносимых элементов
+          int cntToMove = size - i - 1;
+          if (cntToMove > 0) {
+            System.arraycopy(elements, i + 1, elements, i, cntToMove);
+            size--;
+            elements[size] = null;
+            return true;
+          }
+        }
+      }
+    }
+    // элемент не найден
+    return false;
+  }
+
+  /**
+   * Удаляет и возвращает элемент с указанным индексом.
+   */
+  public T remove(int index) {
+    rangeCheck(index);
+
+    T removedElement = (T) elements[index];
+
+    // количество переносимых элементов
+    int cntToMove = size - index - 1;
+    if (cntToMove > 0) {
+      System.arraycopy(elements, index+1, elements, index, cntToMove);
+      size--;
+      elements[size] = null;
+    }
+
+    return removedElement;
+  }
+
+  /**
+   * Проверка на вхождение всех элементов из указанной коллекции в список.
+   */
+  public boolean containsAll(Collection<?> c){
+    if (c == null)
+      throw new NullPointerException();
+
+    Iterator it = c.iterator();
+    while (it.hasNext()) {
+      if (!contains(it.next()))
+        return false;
+    }
+    return true;
+  }
+
+  /**
+   * Добавление элементов коллекции к списку.
+   */
+  public boolean addAll(Collection<? extends T> c){
+    if (c == null)
+      throw new NullPointerException();
+
+    Object[] a = c.toArray();
+    int addedCount = a.length;
+
+    checkAndGrowCapacity(addedCount);
+
+    // расширение elements
+    // добавление новых элементов
+    System.arraycopy(a, 0, elements, size, addedCount);
+
+    size = size + addedCount;
+    return addedCount > 0;
+  }
+
+  /**
+   * Добавление элементов коллекции к списку начиная с элемента с указанным индексом.
+   */
+  public boolean addAll(int i, Collection<? extends T> collection) {
+    Object[] objArray = collection.toArray();
+    int arrLength = objArray.length;
+    checkAndGrowCapacity(this.size + arrLength);
+    int idx = this.size - i;
+    if (idx > 0) {
+      System.arraycopy(this.elements, i, this.elements, i + arrLength, idx);
+    }
+
+    System.arraycopy(objArray, 0, this.elements, i, arrLength);
+    this.size += arrLength;
+    return arrLength != 0;
+  }
+
+  /**
+   * Удаление из списка всех элементов, входящих в указанную коллекцию.
+   */
+  public boolean removeAll(Collection<?> c){
+    if (c == null)
+      throw new NullPointerException();
+
+    boolean modified = false;
+    int idx = 0;
+
+    for (int i = 0; i < size; i++) {
+      if (!c.contains(elements[i])) {
+        elements[idx] = elements[i];
+        idx++;
+        modified = true;
+      }
+    }
+
+    if (idx < size) {
+      for (int i = idx; i < size; i++) {
+        elements[i] = null;
+      }
+    }
+    size = idx;
+
+    return modified;
+  }
+
+  /**
+   * Удаление из списка элементов, не содержащихся в указанной коллекции
+   */
+  public boolean retainAll(Collection<?> c){
+    if (c == null)
+      throw new NullPointerException();
+
+    boolean modified = false;
+    int idx = 0;
+
+    for (int i = 0; i < size; i++) {
+      if (c.contains(elements[i])) {
+        elements[idx] = elements[i];
+        idx++;
+        modified = true;
+      }
+    }
+
+    if (idx < size) {
+      for (int i = idx; i < size; i++) {
+        elements[i] = null;
+      }
+    }
+    size = idx;
+
+    return modified;
+  }
+
+  /**
+   * удаляет все элементы списка
+   */
+  public void clear(){
+    for (int i = 0; i < size; i++ ) {
+      elements[i] = null;
+    }
+    size = 0;
+  }
+
+  /**
+   * Уменьшение длины внутренненго массива до актуального размера списка.
+   */
+  public void trimToSize() {
+    if (size < elements.length) {
+      if (size == 0)
+        elements = EMPTY_DATA;
+      else
+        elements = Arrays.copyOf(elements, size);
+    }
+  }
+
+  /*
+  * Сортировка элементов списка
+  * */
+  public void sort(Comparator<? super T> var1) {
+    Arrays.sort((T[])this.elements, 0, this.size, var1);
+  }
+}
