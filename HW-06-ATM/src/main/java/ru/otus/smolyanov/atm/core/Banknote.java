90e1e64d4579b7124f694dbac142ffc8efca9bc1
SmolyanovGregory <gsm-it@yandex.ru>
2017-12-29 15:36:57 +0300

    Initial version HW-06-ATM

---
+++ HW-06-ATM/src/main/java/ru/otus/smolyanov/atm/core/Banknote.java
@@ -0,0 +1,27 @@
+package ru.otus.smolyanov.atm.core;
+
+/**
+ * Created by Gregory Smolyanov.
+ * <p>
+ * Home work 06 - ATM
+ */
+
+public enum Banknote {
+  FIVE(5),
+  TEN(10),
+  TWENTY(20),
+  FIFTY(50),
+  ONE_HUNDRED(100),
+  TWO_HUNDRED(200),
+  FIVE_HUNDRED(500);
+
+  private final int denomination;
+
+  Banknote(int denomination) {
+    this.denomination = denomination;
+  }
+
+  public int getdDenomination() {
+    return denomination;
+  }
+}
