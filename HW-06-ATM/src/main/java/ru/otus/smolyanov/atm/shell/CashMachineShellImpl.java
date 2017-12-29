494a9cd1b09a0cfa139291c412fddb9c246e60da
SmolyanovGregory <gsm-it@yandex.ru>
2017-12-29 15:38:26 +0300

    Initial version HW-06-ATM

---
+++ HW-06-ATM/src/main/java/ru/otus/smolyanov/atm/shell/CashMachineShellImpl.java
@@ -0,0 +1,71 @@
+package ru.otus.smolyanov.atm.shell;
+
+/**
+ * Created by Gregory Smolyanov.
+ * <p>
+ * Home work 06 - ATM
+ */
+
+import ru.otus.smolyanov.atm.command.CashEntryCommand;
+import ru.otus.smolyanov.atm.command.CashWithdrawalCommand;
+import ru.otus.smolyanov.atm.command.CommandInvoker;
+import ru.otus.smolyanov.atm.command.GetBalanceCommand;
+import ru.otus.smolyanov.atm.core.CashMachine;
+
+import java.io.PrintStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class CashMachineShellImpl implements CashMachineShell {
+
+  private final String COMMAND_BALANCE = "1";
+  private final String COMMAND_WITHDRAWAL = "2";
+  private final String COMMAND_ENTRY = "3";
+
+  private final Scanner scanner;
+  private final PrintStream printStream;
+  private final CashMachine cashMachine;
+  private CommandInvoker commandInvoker;
+
+  public CashMachineShellImpl(CashMachine cashMachine, InputStream inputStream, PrintStream printStream) {
+    this.cashMachine = cashMachine;
+    scanner = new Scanner(inputStream);
+    this.printStream = printStream;
+
+    commandInvoker = new CommandInvoker(
+      new GetBalanceCommand(cashMachine, printStream),
+      new CashWithdrawalCommand(cashMachine, printStream, new Scanner(inputStream)),
+      new CashEntryCommand(cashMachine, printStream, new Scanner(inputStream))
+    );
+  }
+
+  @Override
+  public void start() {
+    cashMachine.start();
+
+    while (true) {
+      // start menu
+      printStream.println("------------------------------\n" +
+          "Type '"+COMMAND_BALANCE+"' for show the balance\n" +
+          "Type '"+COMMAND_WITHDRAWAL+"' for withdrawal cash\n" +
+          "Type '"+COMMAND_ENTRY+"' for entry cash"+
+          "\n------------------------------");
+
+      String command = scanner.nextLine();
+      switch (command) {
+        case COMMAND_BALANCE:
+          commandInvoker.getBalance();
+          break;
+        case COMMAND_WITHDRAWAL:
+          commandInvoker.cashWithdrawal();
+          break;
+        case COMMAND_ENTRY:
+          commandInvoker.cashEntry();
+          break;
+        default:
+          printStream.println("Incorrect command: "+command);
+          break;
+      }
+    }
+  }
+}
